f8c3fae88cdf68817a8743479a8bc6c2ba6e0a69
StevenZheng <haoranz@me.com>
2018-10-20 10:38:44 +0800

    1820

--- frist/fristdemo.py
+++ frist/fristdemo.py
@@ -4,5 +4,15 @@
 print ('hello world');
 
 #for循环
-for i in range(50,100+1):
+for i in range(50,60+1):
     print (i);
+
+#一个简单的函数
+def add_function(a,b):
+    c=a+b
+    return c;
+    
+d=add_function(2,3)
+print (d);
+
+
