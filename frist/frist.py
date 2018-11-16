90b64898f75ecf55160b168df6dfe00ae78bf79e
郑浩然 <zhenghaoran@zhenghaorandeMacBook-Pro.local>
2018-11-16 19:18:31 +0800

    20181116-02

--- frist/frist.py
+++ frist/frist.py
@@ -9,10 +9,20 @@ sys.path.append(o_path)
 from second.second01 import a
 print (a)
 
-#获取系统时间
+#获取系统时间 
+#获取日期差
 import time
+import datetime
 def Getnowtime():
     #return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
-    return time.strftime("%Y-%m-%d",time.localtime(time.time()))
+    return time.strftime("%Y%m%d",time.localtime(time.time()))
 systime=str.format(Getnowtime())
 print(systime)
+
+a=datetime.datetime.strptime(Getnowtime(),'%Y%m%d')
+print (type(a))
+b=datetime.datetime.strptime('2018728','%Y%m%d')
+
+days=(a-b).days
+print (days)
+
